13cb02b918265d27aef706208103cc96f0064e9c
Sarthak Jain <332Sarthak22@gmail.com>
2021-11-14 04:29:44 +0530

    added paths to imgs

--- img_to_float.py
+++ img_to_float.py
@@ -1,7 +1,7 @@
-import cv2
 import numpy as np
 from skimage.util import img_as_float
 
 path = r'C:\Users\acer\Pictures\Acer\Acer_Wallpaper_02_5000x2813.jpg'
-
+path = r'C:\Users\acer\Pictures\Aesthetic images\2803582.jpg'
+path = r'C:\Users\acer\Pictures\Aesthetic images\wp4288365-aesthetic-laptop-wallpapers.jpg'
 image = np.arrange()
